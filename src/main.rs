f40a15c7aaa67fee45f12476ae09b2beef731101
Parth Patel <54561512+protocol66@users.noreply.github.com>
2021-05-01 18:17:22 -0500

    Reverted back to cortex_m CorePeripherals, added semihosting

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate panic_halt; // panic handler
 
 use cortex_m;
 use cortex_m_rt::entry;
+use cortex_m_semihosting::hio;
 use stm32_hal2 as hal;
 
 use crate::hal::{prelude::*,
@@ -13,15 +14,28 @@ use crate::hal::{prelude::*,
                  pac,
 };
 
+// Used with semihosting to print to terminal
+use core::fmt::Write;
+
 #[entry]
 fn main() -> ! {
+    // printing to debug terminal
+    let mut hstdout = hio::hstdout().unwrap();
+    writeln!(hstdout, "Initializing....").ok();
+
+    // Grab Peripherals
     let p = pac::Peripherals::take().unwrap();
-    let cp = pac::CorePeripherals::take().unwrap();
-    
+    let cp = cortex_m::Peripherals::take().unwrap();
+
+    // Setup Clocks. Configured as 80 MHz sysclk and pll using HSI
     let clocks = Clocks::default();
-    clocks.setup(&mut p.RCC, &mut p.FLASH);
-    
-    loop{
+    clocks.setup(&mut p.RCC, &mut p.FLASH).ok();
+
+    // Rest of setup goes here....
+
+    writeln!(hstdout, "done.").ok();
+
+    loop {
 
     }
 }
